fe1b3582ade2f657cd832c11cab6e7df4ec02978
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-10-21 11:11:40 +0100

    Initial code commit

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-node-unrtf
+# node-unrtf
+
+[![GitHub Release](https://img.shields.io/github/release/Fdawgs/node-unrtf.svg)](https://github.com/Fdawgs/node-unrtf/releases/latest/) [![npm version](https://img.shields.io/npm/v/node-unrtf)](https://www.npmjs.com/package/node-unrtf)
+[![Build Status](https://travis-ci.com/Fdawgs/node-unrtf.svg?branch=master)](https://travis-ci.com/Fdawgs/node-unrtf) [![Coverage Status](https://coveralls.io/repos/github/Fdawgs/node-unrtf/badge.svg?branch=master)](https://coveralls.io/github/Fdawgs/node-unrtf?branch=master) [![Known Vulnerabilities](https://snyk.io/test/github/Fdawgs/node-unrtf/badge.svg)](https://snyk.io/test/github/Fdawgs/node-unrtf) [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+
+> Asynchronous node.js wrapper for the UnRTF RTF conversion program
+
+## Intro
+
+The node-unrtf module was created out of a need for a RTF-to-HTML conversion module at [Yeovil District Hospital NHSFT](https://yeovilhospital.co.uk/) to convert clinical documents in RTF format to HTML.
+
+## Installation
+
+### Linux support
+
+A Windows binary (v0.19.3) is provided with this repository.
+For Linux users, you will need to download the `unrtf` binary separately.
+
+An example of downloading the binary on a Debian system:
+
+```
+sudo apt-get install unrtf
+```
+
+Once they have been installed, you will need to pass the `unrtf` installation directory in as parameters to an instance of the Poppler class:
+
+```js
+const { UnRTF } = require('node-unrtf');
+const unRtf = new UnRTF('./usr/bin');
+```
+
+## Contributing
+
+Please see [CONTRIBUTING.md](https://github.com/Fdawgs/node-unrtf/blob/master/CONTRIBUTING.md) for more details regarding contributing to this project.
+
+## License
+
+`node-unrtf` is licensed under the [MIT](https://github.com/Fdawgs/node-unrtf/blob/master/LICENSE) license.
---
+++ src/index.js
@@ -0,0 +1,123 @@
+const os = require('os');
+const path = require('path');
+const { execFile } = require('child_process');
+const util = require('util');
+
+const execFileSync = util.promisify(execFile);
+const platform = os.platform();
+
+/**
+ * @author Frazer Smith
+ * @description Check each option provided is valid and of the correct type.
+ * @param {object} options - Object containing options to pass to binary.
+ * @param {object} acceptedOptions - Object containing options that a binary accepts.
+ * @returns {Promise<string|Error>} Promise of stdout string on resolve, or Error object on rejection.
+ */
+function parseOptions(options, acceptedOptions) {
+	return new Promise((resolve, reject) => {
+		const args = [];
+		Object.keys(options).forEach((key) => {
+			if (Object.prototype.hasOwnProperty.call(acceptedOptions, key)) {
+				// eslint-disable-next-line valid-typeof
+				if (typeof options[key] === acceptedOptions[key].type) {
+					args.push(acceptedOptions[key].arg);
+					if (typeof options[key] !== 'boolean') {
+						args.push(options[key]);
+					}
+				} else {
+					reject(
+						new Error(
+							`Invalid value type provided for option '${key}', expected ${
+								acceptedOptions[key].type
+							} but recieved ${typeof options[key]}`
+						)
+					);
+				}
+			} else {
+				reject(new Error(`Invalid option provided '${key}'`));
+			}
+		});
+		resolve(args);
+	});
+}
+
+class UnRTF {
+	/**
+	 * @param {string=} binPath - Path of UnRTF binary.
+	 * Useful for Linux users who have UnRTF binary already installed.
+	 */
+	constructor(binPath) {
+		if (binPath) {
+			this.unrtfPath = binPath;
+		} else {
+			let unrtfPath;
+
+			// Build path to UnRTF binary based on OS
+			switch (platform) {
+				// Windows OS
+				case 'win32':
+					unrtfPath = path.join(
+						__dirname,
+						'lib',
+						'win32',
+						'unrtf-0.19.3',
+						'bin'
+					);
+					break;
+				default:
+					return new Error(`${platform} is NOT supported.`);
+			}
+
+			this.unrtfPath = unrtfPath;
+		}
+	}
+
+	/**
+	 * @author Frazer Smith
+	 * @description Converts a RTF file to HTML/LaTeX/RTF/TXT.
+	 * UnRTF will use the directory of the original file to store embedded pictures.
+	 * @param {object=} options - Object containing options to pass to binary.
+	 * @param {boolean=} options.noPictures - Disable the automatic storing of embedded
+	 * pictures to the directory of the original file.
+	 * @param {boolean=} options.noRemap - Disable charset conversion (only works for 8-bit charsets).
+	 * @param {boolean=} options.outputHtml - Generate HTML output.
+	 * @param {boolean=} options.outputLatex - Generate LaTeX output.
+	 * @param {boolean=} options.outputRtf - Generate RTF output.
+	 * @param {boolean=} options.outputText - Generate ASCII text output.
+	 * @param {boolean=} options.outputVt - Generate text output with VT100 escape codes.
+	 * @param {boolean=} options.printVersionInfo - Print copyright and version info.
+	 * @param {boolean=} options.quiet - Do not print any messages or errors.
+	 * @param {string} file - Filepath of the RTF file to read.
+	 * @returns {Promise<string|Error>} Promise of stdout string on resolve, or Error object on rejection.
+	 */
+	async convert(options = {}, file) {
+		const acceptedOptions = {
+			noPictures: { arg: '--nopict', type: 'boolean' },
+			noRemap: { arg: '--noremap', type: 'boolean' },
+			outputHtml: { arg: '--html', type: 'boolean' },
+			outputLatex: { arg: '--latex', type: 'boolean' },
+			outputRtf: { arg: '--rtf', type: 'boolean' },
+			outputText: { arg: '--text', type: 'boolean' },
+			outputVt: { arg: '--vt', type: 'boolean' },
+			quiet: { arg: '--quiet', type: 'boolean' },
+			printVersionInfo: { arg: '--version', type: 'boolean' }
+		};
+
+		try {
+			const args = await parseOptions(options, acceptedOptions);
+			args.push(file);
+
+			const { stdout } = await execFileSync(
+				path.join(this.unrtfPath, 'unrtf'),
+				args
+			);
+			return stdout;
+		} catch (err) {
+			return err;
+		}
+	}
+}
+
+module.exports = {
+	UnRTF
+};
