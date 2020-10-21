8bc8c289bf8f2b3435e1867a6ba9a2f0046c94a2
Frazer Smith <frazer.dev@outlook.com>
2020-10-21 14:00:48 +0100

    docs(index): clarify on required unrtf binary version

--- src/index.js
+++ src/index.js
@@ -79,18 +79,17 @@ class UnRTF {
 	 * @param {object=} options - Object containing options to pass to binary.
 	 * @param {boolean=} options.noPictures - Disable the automatic storing of embedded
 	 * pictures to the directory of the original file.
-	 * @param {boolean=} options.noRemap - Disable charset conversion (only works for 8-bit charsets).
+	 * @param {boolean=} options.noRemap - Disable charset conversion (only works for 8-bit charsets)
+	 * (UnRTF v0.20.5 or later only).
 	 * @param {boolean=} options.outputHtml - Generate HTML output.
 	 * @param {boolean=} options.outputLatex - Generate LaTeX output.
-	 * @param {boolean=} options.outputPs - Generate PostScript (PS) output.
-	 * Will error if used with UnRTF v0.19.4 or later.
-	 * @param {boolean=} options.outputRtf - Generate RTF output.
+	 * @param {boolean=} options.outputPs - Generate PostScript (PS) output (UnRTF v0.19.4 or earlier only). 
+	 * @param {boolean=} options.outputRtf - Generate RTF output. (UnRTF v0.21.3 or later only).
 	 * @param {boolean=} options.outputText - Generate ASCII text output.
 	 * @param {boolean=} options.outputVt - Generate text output with VT100 escape codes.
-	 * @param {boolean=} options.outputWpml - Generate WPML output.
-	 * Will error if used with UnRTF v0.19.4 or later.
+	 * @param {boolean=} options.outputWpml - Generate WPML output (UnRTF v0.19.4 or earlier only).
 	 * @param {boolean=} options.printVersionInfo - Print copyright and version info.
-	 * @param {boolean=} options.quiet - Do not print any messages or errors.
+	 * @param {boolean=} options.quiet - Do not print any leading comments in output (UnRTF v0.21.3 or later only).
 	 * @param {string} file - Filepath of the RTF file to read.
 	 * @returns {Promise<string|Error>} Promise of stdout string on resolve, or Error object on rejection.
 	 */
