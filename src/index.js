4a9816bfda1de634eb6b774dc426e8ea017a1e86
Frazer Smith <frazer.dev@outlook.com>
2023-09-25 08:31:43 +0100

    perf(index): create `acceptedOptions` obj on init, not every func call

--- src/index.js
+++ src/index.js
@@ -127,32 +127,9 @@ class UnRTF {
 		]).stderr.toString();
 		/** @type {string|undefined} */
 		this.unrtfVersion = /^(\d{1,2}\.\d{1,2}\.\d{1,2})/u.exec(version)?.[1];
-	}
 
-	/**
-	 * @author Frazer Smith
-	 * @description Converts an RTF file to HTML/LaTeX/RTF/TXT.
-	 * Defaults to HTML output if no `output*` options are provided.
-	 * UnRTF will use the directory of the original file to store embedded pictures.
-	 * @param {string} file - Filepath of the RTF file to read.
-	 * @param {object} [options] - Object containing options to pass to binary.
-	 * @param {boolean} [options.noPictures] - Disable the automatic storing of embedded
-	 * pictures to the directory of the original file.
-	 * @param {boolean} [options.noRemap] - Disable charset conversion (only works for 8-bit charsets)
-	 * (UnRTF v0.20.5 or later only).
-	 * @param {boolean} [options.outputHtml] - Generate HTML output.
-	 * @param {boolean} [options.outputLatex] - Generate LaTeX output.
-	 * @param {boolean} [options.outputPs] - Generate PostScript (PS) output (UnRTF v0.19.4 or earlier only).
-	 * @param {boolean} [options.outputRtf] - Generate RTF output. (UnRTF v0.21.3 or later only).
-	 * @param {boolean} [options.outputText] - Generate ASCII text output.
-	 * @param {boolean} [options.outputVt] - Generate text output with VT100 escape codes.
-	 * @param {boolean} [options.outputWpml] - Generate WPML output (UnRTF v0.19.4 or earlier only).
-	 * @param {boolean} [options.printVersionInfo] - Print copyright and version info.
-	 * @param {boolean} [options.quiet] - Do not print any leading comments in output (UnRTF v0.21.3 or later only).
-	 * @returns {Promise<string>}  A promise that resolves with a stdout string, or rejects with an `Error` object.
-	 */
-	async convert(file, options = {}) {
-		const acceptedOptions = {
+		/** @type {object} */
+		this.unrtfAcceptedOptions = {
 			noPictures: {
 				arg: "--nopict",
 				type: "boolean",
@@ -195,7 +172,31 @@ class UnRTF {
 			},
 			quiet: { arg: "--quiet", type: "boolean", minVersion: "0.21.3" },
 		};
+	}
 
+	/**
+	 * @author Frazer Smith
+	 * @description Converts an RTF file to HTML/LaTeX/RTF/TXT.
+	 * Defaults to HTML output if no `output*` options are provided.
+	 * UnRTF will use the directory of the original file to store embedded pictures.
+	 * @param {string} file - Filepath of the RTF file to read.
+	 * @param {object} [options] - Object containing options to pass to binary.
+	 * @param {boolean} [options.noPictures] - Disable the automatic storing of embedded
+	 * pictures to the directory of the original file.
+	 * @param {boolean} [options.noRemap] - Disable charset conversion (only works for 8-bit charsets)
+	 * (UnRTF v0.20.5 or later only).
+	 * @param {boolean} [options.outputHtml] - Generate HTML output.
+	 * @param {boolean} [options.outputLatex] - Generate LaTeX output.
+	 * @param {boolean} [options.outputPs] - Generate PostScript (PS) output (UnRTF v0.19.4 or earlier only).
+	 * @param {boolean} [options.outputRtf] - Generate RTF output. (UnRTF v0.21.3 or later only).
+	 * @param {boolean} [options.outputText] - Generate ASCII text output.
+	 * @param {boolean} [options.outputVt] - Generate text output with VT100 escape codes.
+	 * @param {boolean} [options.outputWpml] - Generate WPML output (UnRTF v0.19.4 or earlier only).
+	 * @param {boolean} [options.printVersionInfo] - Print copyright and version info.
+	 * @param {boolean} [options.quiet] - Do not print any leading comments in output (UnRTF v0.21.3 or later only).
+	 * @returns {Promise<string>}  A promise that resolves with a stdout string, or rejects with an `Error` object.
+	 */
+	async convert(file, options = {}) {
 		/**
 		 * UnRTF will attempt to convert empty strings, missing files,
 		 * and non-RTF files. Catch before passing to binary
@@ -214,7 +215,11 @@ class UnRTF {
 			);
 		}
 
-		const args = parseOptions(acceptedOptions, options, this.unrtfVersion);
+		const args = parseOptions(
+			this.unrtfAcceptedOptions,
+			options,
+			this.unrtfVersion
+		);
 		args.push(normalizeTrim(file));
 
 		return new Promise((resolve, reject) => {
