c395cbce0ddb73dfbdbdc4a5710071833b2c1976
Frazer Smith <frazer.dev@icloud.com>
2025-10-08 15:44:51 +0100

    test(utils): use template for types

--- test_resources/utils/bt-power-set.js
+++ test_resources/utils/bt-power-set.js
@@ -2,11 +2,12 @@
 
 /**
  * @description Generates the power set of a given set using a recursive backtracking approach.
- * @param {*[]} originalSet - Original set of elements we are forming power-set of.
- * @param {*[][]} [allSubsets] - All subsets that have been formed so far. Used internally for recursion.
- * @param {*[]} [currentSubSet] - The current subset being formed. Used internally for recursion.
+ * @template T
+ * @param {T[]} originalSet - Original set of elements we are forming power-set of.
+ * @param {T[][]} [allSubsets] - All subsets that have been formed so far. Used internally for recursion.
+ * @param {T[]} [currentSubSet] - The current subset being formed. Used internally for recursion.
  * @param {number} [startAt] - The position in the original set from which to start forming the current subset.
- * @returns {*[][]} All subsets of original set.
+ * @returns {T[][]} All subsets of original set.
  */
 function btPowerSetRecursive(
 	originalSet,
